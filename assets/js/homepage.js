088258ada62174cd6d0aebc98228edebba4c864f
Lucca Araujo <lucca.ara7@gmail.com>
2022-03-26 18:37:18 -0700

    add languages filter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function(user){
     //format the github api url
@@ -96,6 +97,21 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
             }
   };
+
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    });
+  };
+
 //var getUserRepos = function(){
     //fetch("https://api.github.com/users/octocat/repos").then(function(response){
     //response.json().then(function(data){
@@ -106,3 +122,15 @@ var displayRepos = function(repos, searchTerm) {
 
 //console.log("outside");
 //getUserRepos()
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language");
+  console.log(language)
+  if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
+}
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
